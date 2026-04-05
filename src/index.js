3172708314d0b3c8a6014f5d69d9b29b7c04e07e
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-04-05 00:50:34 -0500

    build error responses

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import { Hono } from 'hono'
+import projects from './routes/projects.js'
+import { isApiError } from './utils/errors.js'
 
 const app = new Hono()
+const api = new Hono()
+
+app.use('*', async (c, next) => {
+  c.set('traceId', crypto.randomUUID())
+  await next()
+})
+
+api.route('/projects', projects)
+
+app.route('/api', api)
+
+app.onError((error, c) => {
+  if (isApiError(error)) {
+    return sendError(c, error.status, error.code, error.message, error.details)
+  }
+
+  console.error('Unhandled error:', error)
+  return sendError(
+    c,
+    500,
+    'INTERNAL_SERVER_ERROR',
+    'An unexpected server error occurred.',
+  )
+})
 
 export default app
